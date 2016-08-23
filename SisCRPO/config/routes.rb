d499f24eee83010e6a629c98e5f85bb40ab362b3
IA <c4rlo4@gmail.com>
2016-08-23 17:49:05 -0300

    OPM pronto

--- SisCRPO/config/routes.rb
+++ SisCRPO/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :gpm
 
-  resources :users, :only => [:index, :new, :create, :edit, :destroy, :update]
+  resources :users, :only => [:new, :create, :edit, :destroy, :update]
 
   devise_for :users
 
