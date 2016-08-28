8239a17c3e10456e699ae850c47e450f34826d2c
demetriusteles <luisdemetrius@gmail.com>
2016-08-28 19:58:36 -0300

    validations company

--- SisCRPO/app/models/company.rb
+++ SisCRPO/app/models/company.rb
@@ -1,7 +1,9 @@
 class Company < ActiveRecord::Base
-	validates :nome, uniqueness: true, presence: true
+	validates :nome, presence: true
 	validates :ativo, presence: true
 	validates :cidade, presence: true
 	validates :unidade, presence: true
-	validates :numero_servidor, presence: true, numericality: true
+	validates :numero_servidor, presence: true
+	validates_uniqueness_of :nome, message: "Já Cadastrado na base"
+	validates_numericality_of :numero_servidor , message: "O campo número de servidor deve ser numérico"
 end
