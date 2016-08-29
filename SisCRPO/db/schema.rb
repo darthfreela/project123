f31588a09042a1c5cfa63174f9943ecc9f50b215
demetriusteles <luisdemetrius@gmail.com>
2016-08-28 23:03:41 -0300

    revisão

--- SisCRPO/db/schema.rb
+++ SisCRPO/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160828151021) do
+ActiveRecord::Schema.define(version: 20160829013149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,9 @@ ActiveRecord::Schema.define(version: 20160828151021) do
     t.string   "nome"
     t.string   "cidade"
     t.string   "unidade"
-    t.integer  "numero_servidor"
     t.boolean  "ativo"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "estagiarios", force: :cascade do |t|
@@ -124,11 +123,12 @@ ActiveRecord::Schema.define(version: 20160828151021) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "siglas", force: :cascade do |t|
+  create_table "postograduacaos", force: :cascade do |t|
     t.string   "sigla"
-    t.string   "nome"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "nomePostoGraduacao"
+    t.boolean  "ativo"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "solicitacao_fers", force: :cascade do |t|
@@ -138,16 +138,6 @@ ActiveRecord::Schema.define(version: 20160828151021) do
     t.boolean  "aprovado"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-  end
-
-  create_table "tipo_servicos", force: :cascade do |t|
-    t.string   "sigla"
-    t.string   "nome"
-    t.integer  "idFuncao"
-    t.integer  "idUniformes"
-    t.boolean  "ativo"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "uniforms", force: :cascade do |t|
