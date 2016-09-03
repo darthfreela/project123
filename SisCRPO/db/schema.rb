013c2156e8fcdf4c8ed13caa1008502584ace655
IA <c4rlo4@gmail.com>
2016-09-03 20:34:46 -0300

    adicionando campo

--- SisCRPO/db/schema.rb
+++ SisCRPO/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903182918) do
+ActiveRecord::Schema.define(version: 20160903190516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160903182918) do
     t.integer  "vale_transporte"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.date     "vigencia_contrato"
   end
 
   create_table "ferias", force: :cascade do |t|
@@ -159,6 +160,13 @@ ActiveRecord::Schema.define(version: 20160903182918) do
     t.datetime "updated_at",         null: false
   end
 
+  create_table "siglas", force: :cascade do |t|
+    t.string   "sigla"
+    t.string   "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "solicitacao_fers", force: :cascade do |t|
     t.integer  "id_func"
     t.date     "data_inicial"
@@ -177,6 +185,16 @@ ActiveRecord::Schema.define(version: 20160903182918) do
     t.integer  "nrBoletim"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "tipo_servicos", force: :cascade do |t|
+    t.string   "sigla"
+    t.string   "nome"
+    t.integer  "idFuncao"
+    t.integer  "idUniformes"
+    t.boolean  "ativo"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "uniforms", force: :cascade do |t|
