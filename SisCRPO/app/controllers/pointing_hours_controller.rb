ae8bf8c2312dc800ad8f6c3d2bf15ab7c0cccabb
demetriusteles <luisdemetrius@gmail.com>
2016-09-04 14:41:54 -0300

    erro

--- SisCRPO/app/controllers/pointing_hours_controller.rb
+++ SisCRPO/app/controllers/pointing_hours_controller.rb
@@ -9,6 +9,7 @@ def new
     @pointingHour = PointingHour.new(pointingHour_params)
     @pointingHour_show = PointingHour.all
     if @pointingHour.save
+      @user = User.new(@user.id, :idFunc)
         redirect_to pointing_hours_new_path , notice: "Apontamento de Horas cadastrado com sucesso."
       else
         render action: :new
@@ -22,6 +23,7 @@ end
 def update
       @pointingHour = PointingHour.find(params[:id])
       if @pointingHour.update_attributes(pointingHour_params)
+          @user = User.new(@user.id, :idFunc)
            redirect_to pointing_hours_new_path, notice: "Apontamento de Horas editado com sucesso."
       else
 
@@ -37,6 +39,6 @@ end
 
   private
   def pointingHour_params
-    params.require(:pointingHour).permit(:id_func, :data_inicio, :hora_inicio, :data_fim, :hora_fim)
+    params.require(:pointingHour).permit(:idfunc, :data_inicio, :hora_inicio, :data_fim, :hora_fim)
   end
 end
--- SisCRPO/app/models/pointing_hour.rb
+++ SisCRPO/app/models/pointing_hour.rb
@@ -1,3 +1,3 @@
 class PointingHour < ActiveRecord::Base
-
+	has_many :users
 end
