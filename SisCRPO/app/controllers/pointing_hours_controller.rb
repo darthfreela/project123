60028dd5727cda629f0a86685c07419788e4d61e
eversclei <evers.clei@gmail.com>
2016-09-05 20:46:23 -0300

    horas

--- SisCRPO/app/controllers/pointing_hours_controller.rb
+++ SisCRPO/app/controllers/pointing_hours_controller.rb
@@ -10,7 +10,6 @@ def new
     @pointingHour = PointingHour.new(pointingHour_params)
     @pointingHour_show = PointingHour.all
     if @pointingHour.save
-      @user = User.new(@user.id, :idFunc)
         redirect_to pointing_hours_new_path , notice: "Apontamento de Horas cadastrado com sucesso."
       else
         render action: :new
@@ -19,12 +18,13 @@ def new
 
 def edit
     @pointingHour = PointingHour.find(params[:id])
+    @tpServico = TipoServico.all
 end
 
 def update
       @pointingHour = PointingHour.find(params[:id])
       if @pointingHour.update_attributes(pointingHour_params)
-          @user = User.new(@user.id, :idFunc)
+        @tpServico = TipoServico.all
            redirect_to pointing_hours_new_path, notice: "Apontamento de Horas editado com sucesso."
       else
 
@@ -40,7 +40,7 @@ end
 
   private
   def pointingHour_params
-    params.require(:pointingHour).permit(:idfunc, :data_inicio, :hora_inicio, :data_fim, :hora_fim, :tipo_servico_id)
+    params.require(:pointing_hour).permit(:idfunc, :data_inicio, :hora_inicio, :data_fim, :hora_fim, :tipo_servico_id)
   end
 end
 
