c5d11fe8da9cc35aa59510d06e8d6487967f4be9
demetriusteles <luisdemetrius@gmail.com>
2016-10-02 21:30:33 -0300

    erro

--- SisCRPO/app/controllers/imported_files_controller.rb
+++ SisCRPO/app/controllers/imported_files_controller.rb
@@ -1,36 +1,36 @@
 class ImportedFileController < ApplicationController
-  def new
-    @imported_files = ImportedFile.new
-    @imported_files_show = ImportedFile.all
-  end
+  	def new
+    	@importedFile = ImportedFile.new
+    	@importedFile_show = ImportedFile.all
+  	end
 
-  def create
-    @imported_files = ImportedFile.new(imported_file_params)
-    @imported_files_show = imported_file.all
-  end
+  	def create
+    	@importedFile = ImportedFile.new(importedFile_params)
+    	@importedFile_show = ImportedFile.all
+  	end
 
-def edit
-    @imported_files = ImportedFile.find(params[:id])
-end
+	def edit
+    	@importedFile = ImportedFile.find(params[:id])
+	end
 
-def update
-      @imported_files = ImportedFile.find(params[:id])
-      if @imported_files.update_attributes(imported_file_params)
+	def update
+      	@importedFile = ImportedFile.find(params[:id])
+      	if @importedFile.update_attributes(importedFile_params)
            redirect_to new_imported_file_path, notice: "Arquivo importatdo com suscesso."
-      else
+      	else
 
             redirect_to new_imported_file_path, :flash => { :error => "Erro ao Importar Arquivo" }
-      end
-end
+      	end
+	end
 
-def destroy
-      @imported_files = ImportedFile.find(params[:id])
-      @profile.destroy
-      redirect_to new_imported_file_path, notice: "Arquivo removido com sucesso."
-end
+	def destroy
+      	@importedFile = ImportedFile.find(params[:id])
+      	@importedFile.destroy
+      	redirect_to new_imported_file_path, notice: "Arquivo removido com sucesso."
+	end
 
-  private
-  def imported_file_params
-    params.require(:imported_file).permit()
-  end
+  	private
+  	def importedFile_params
+    	params.require(:imported_files).permit()
+  	end
 end
