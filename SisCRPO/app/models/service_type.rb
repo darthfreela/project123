dd4d8b8d51be861f667ba94d537cc21b173c7d5b
IA <c4rlo4@gmail.com>
2016-10-13 13:52:27 -0300

    tipos de serviço OK

--- SisCRPO/app/models/service_type.rb
+++ SisCRPO/app/models/service_type.rb
@@ -1,8 +1,8 @@
 class ServiceType < ActiveRecord::Base
-  validates :nome, presence: true, length: {maximum: 80}
-  validates :sigla, presence: true, length: {minimum:3, maximum: 6}
-  validates :ativo, presence: true
-  validates :idFuncao, numericality:true
-  validates :idUniformes, numericality:true
+  validates :name, presence: true, length: {maximum: 80}
+  validates :initials, presence: true, length: {minimum:3, maximum: 6}
+  validates :actived, presence: true
+  validates :function_id, numericality:true
+  validates :uniform_id, numericality:true
   has_many :pointing_hours
 end
