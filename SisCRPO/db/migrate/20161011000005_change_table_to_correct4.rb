82ad46acdd616495392158dbd767ae9ba02acbb5
eversclei <evers.clei@gmail.com>
2016-10-24 17:01:57 -0200

    Clei Atualização

--- SisCRPO/db/migrate/20161011000005_change_table_to_correct4.rb
+++ SisCRPO/db/migrate/20161011000005_change_table_to_correct4.rb
@@ -1,5 +1,5 @@
 class ChangeTableToCorrect4 < ActiveRecord::Migration
   def change
-    add_column :request_solicitations, :type_solicitation, :integer
+    #add_column :request_solicitations, :type_solicitation, :integer
   end
 end
