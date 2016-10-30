fe2813159169ba0cb2434b43218fee4b4e764fac
dsribeiro276 <dsribeiro276@gmail.com>
2016-10-30 12:04:18 -0200

    roda o PULL antes de mais nada.... '-', aprovações, funcionando com hierarquia de superiores...

--- SisCRPO/app/controllers/approval_solicitation_controller.rb
+++ SisCRPO/app/controllers/approval_solicitation_controller.rb
@@ -1,31 +1,58 @@
 class ApprovalSolicitationController < ApplicationController
   def new
-    @approval_solicitations = ApprovalSolicitation.where(manager_function_id: current_user.function_id).all
+    @approval_solicitations = ApprovalSolicitation.where(
+      manager_function_id: current_user.function_id,status: [0,1,3] ).all
     # # @approval_solicitations = ApprovalSolicitation.joins(:user => [:function]).all
     # @approval_solicitations = ApprovalSolicitation.joins(:user).joins(:function)
     # @approval_solicitations = ApprovalSolicitation.where(user.function.manager_function_id: current_user.function_id)
-    puts("***************************************")
+    # puts("***************************************")
     # puts(@approval_solicitations)
-    puts("***************************************")
+    # puts("***************************************")
     # @solicitations = Solicitation.where(approved: [nil, 3]).all
   end
 
   def edit
     @approval_solicitation = ApprovalSolicitation.find(params[:id])
     @approval_solicitation.update(:status => 1 ,:user_id => current_user.id)
+    puts("***********************************")
+    puts("FUNCTION ID DO PROXIMO")
+    puts(@approval_solicitation.user.function.manager_function_id)
+    puts("***********************************")
+    puts("***********************************")
+
+
+
   end
 
   def reprove
+    """
+        Se o usuario superior NEGAR, a solicitação não vai para o próximo,
+        e ja retorna para o usuario solicitante...
+    """
     @toApprove = ApprovalSolicitation.find(params[:id])
     @toApprove.update(:status => 3 ,:user_id => current_user.id)
-    # @toApprove.update(:approved => 1)
     redirect_to new_approval_solicitation_path, notice: "Solicitação negada com sucesso."
   end
   def approve
+    """
+        Quando o superior aprovar verificar se tem uma função superior,
+        se tem gerar um novo registro na tabela de aprovaçções com o mesmo registro,
+        só alterando o function_id do próximo...
+    """
       @toApprove = ApprovalSolicitation.find(params[:id])
       @toApprove.update(:status => 2 ,:user_id => current_user.id)
-      # @toApprove.update(:approved => 2)
-      redirect_to new_approval_solicitation_path, notice: "Solicitação aprovada com sucesso."
-  end
 
+        if current_user.function_id != @toApprove.user.function.manager_function_id
+          @approval_solicitation = ApprovalSolicitation.new
+          @approval_solicitation.solicitation_id =  @toApprove.solicitation_id
+          @approval_solicitation.status = 0
+          @approval_solicitation.manager_function_id =  @toApprove.user.function.manager_function_id
+          @approval_solicitation.save
+          redirect_to new_approval_solicitation_path,
+           notice: "Solicitação aprovada com sucesso, e enviada para o próximo superior."
+         else
+           redirect_to new_approval_solicitation_path,
+            notice: "Solicitação aprovada com sucesso."
+         end
+  end
 end
