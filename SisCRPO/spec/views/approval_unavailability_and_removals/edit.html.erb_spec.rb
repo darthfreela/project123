97d4a3884b9cca695513d5f5cdd2e13e1919e042
dsribeiro276 <dsribeiro276@gmail.com>
2016-11-07 20:29:40 -0200

    deletado todos os arquivos desnecessários relacionados as solicitações

--- SisCRPO/spec/views/approval_unavailability_and_removals/edit.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "approval_unavailability_and_removals/edit.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- SisCRPO/spec/views/solicitacao_fers/new.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "solicitacao_fers/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
