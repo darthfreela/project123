d270b659a0520a3d66014ce0323c965868c7c2e1
eversclei <evers.clei@gmail.com>
2016-11-07 20:18:40 -0200

    Ajustes nas views de horas

--- SisCRPO/config/routes.rb
+++ SisCRPO/config/routes.rb
@@ -78,9 +78,7 @@ resources :report_overtime_supplementations, :only =>  [:index]
 
   resources :request_dispenses
 
-  resources :disthoraext
-
-  resources :distribuicao_horas_extras, :only =>  [:new, :create, :edit, :destroy, :update]
+  resources :extra_hour_distributions, :only =>  [:new, :create, :edit, :destroy, :update]
 
   resources :profiles, :only =>  [:new, :create, :edit, :destroy, :update]
 
