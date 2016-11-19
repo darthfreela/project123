935683167f6946d761a10514eedf9c686537c621
IA <c4rlo4@gmail.com>
2016-11-19 15:15:15 -0200

    tela envio de foto

--- SisCRPO/app/controllers/user_preferences_controller.rb
+++ SisCRPO/app/controllers/user_preferences_controller.rb
@@ -3,6 +3,11 @@ class UserPreferencesController < ApplicationController
     @user = User.find(current_user.id)
     @post_graduation = PostGraduation.find(current_user.post_graduation_id)
     @city = City.find(@user.city_id)
+
+    @select_cities = City.all
+
+
+
   end
 
   private
