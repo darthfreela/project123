12ff4e3d49279f4017883e65d3102507cf836f6e
IA <c4rlo4@gmail.com>
2016-11-21 16:48:24 -0200

    salvando as atualizações de user

--- SisCRPO/app/controllers/user_preferences_controller.rb
+++ SisCRPO/app/controllers/user_preferences_controller.rb
@@ -8,18 +8,25 @@ class UserPreferencesController < ApplicationController
   end
 
   def save_preferences
+    @user = User.find(current_user.id)
     @address = params[:address]
     @zip_code = params[:zip_code]
     @neighborhood = params[:neighborhood]
     @city_id = params[:city_id]
     @picture = params[:picture]
-    File.open(Rails.root.join('app','assets', 'images', @picture.original_filename), 'wb') do |f|
+    File.open(Rails.root.join('app','assets', 'images', @user.name + "_img_profile"), 'wb') do |f|
         f.write(@picture.read)
      end
-    redirect_to dashboard_index_path notice: @picture.original_filename
+    redirect_to dashboard_index_path notice: "Alterações salvas com sucesso!"
   end
 
   private
-
+  def save_params(address, zip_code, neigborhood, city_id, picture)
+    User.update(current_user, :address => address,
+                                             :zip_code => zip_code,
+                                             :neigborhood => neigborhood,
+                                             :city_id => city_id,
+                                             :image_path => Rails.root.join('app','assets', 'images', 'img_profiles', @user.name + "_img_profile"))
+  end
 
 end
