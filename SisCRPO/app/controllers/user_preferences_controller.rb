553bf31304fd4904bee2c9b4ff8549cbbcad460b
IA <c4rlo4@gmail.com>
2016-11-21 18:22:26 -0200

    Preferencias do usuário %, s só falta um sinalizador de que a imagem foi uploaded

--- SisCRPO/app/controllers/user_preferences_controller.rb
+++ SisCRPO/app/controllers/user_preferences_controller.rb
@@ -3,7 +3,6 @@ class UserPreferencesController < ApplicationController
     @user = User.find(current_user.id)
     @post_graduation = PostGraduation.find(current_user.post_graduation_id)
     @city = City.find(@user.city_id)
-
     @select_cities = City.all
   end
 
@@ -14,19 +13,22 @@ class UserPreferencesController < ApplicationController
     @neighborhood = params[:neighborhood]
     @city_id = params[:city_id]
     @picture = params[:picture]
-    File.open(Rails.root.join('app','assets', 'images', @user.name + "_img_profile"), 'wb') do |f|
+    File.open(Rails.root.join('app','assets', 'images', @user.name + "_img_profile.jpg"), 'wb') do |f|
         f.write(@picture.read)
-     end
-    redirect_to dashboard_index_path notice: "Alterações salvas com sucesso!"
+    end
+
+    if save_params(@address, @zip_code, @neighborhood, @city_id)
+        redirect_to dashboard_index_path notice: "Alterações salvas com sucesso!"
+    end
   end
 
   private
-  def save_params(address, zip_code, neigborhood, city_id, picture)
-    User.update(current_user, :address => address,
+  def save_params(address, zip_code, neighborhood, city_id)
+    User.update(current_user.id, :address => address,
                                              :zip_code => zip_code,
-                                             :neigborhood => neigborhood,
+                                             :neighborhood => neighborhood,
                                              :city_id => city_id,
-                                             :image_path => Rails.root.join('app','assets', 'images', 'img_profiles', @user.name + "_img_profile"))
+                                             :image_path => "/assets/" +  @user.name + "_img_profile.jpg")
   end
 
 end
