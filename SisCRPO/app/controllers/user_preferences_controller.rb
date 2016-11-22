4db11cd75a1c8b1abfda04f25d4d69715fd7b02a
IA <c4rlo4@gmail.com>
2016-11-22 16:18:44 -0200

    alterações do nome da imagem para hash

--- SisCRPO/app/controllers/user_preferences_controller.rb
+++ SisCRPO/app/controllers/user_preferences_controller.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class UserPreferencesController < ApplicationController
   def index
     @user = User.find(current_user.id)
@@ -14,7 +16,10 @@ class UserPreferencesController < ApplicationController
     @city_id = params[:city_id]
     if params[:picture] != nil
         @picture = params[:picture]
-        File.open(Rails.root.join('app','assets', 'images', @user.name + "_img_profile.jpg"), 'wb') do |f|
+        @picture_string = "#{@user.name} #{@user.id} #{@user.war_name}"
+        @picture_hash = Digest::SHA1.hexdigest @picture_string
+
+        File.open(Rails.root.join('app','assets', 'images', "img_profile_#{@picture_hash}.jpg"), 'wb') do |f|
             f.write(@picture.read)
         end
         save_image_profile
@@ -23,26 +28,30 @@ class UserPreferencesController < ApplicationController
 
     if save_params(@address, @zip_code, @neighborhood, @city_id)
         redirect_to dashboard_index_path notice: "Alterações salvas com sucesso!"
+    else
+        redirect_to user_preferences_index_path notice: "Não foi possível editar as preferencias de usuário."
     end
   end
 
   private
   def save_params(address, zip_code, neighborhood, city_id)
+    flag = true
     if address != ""
-        User.update(current_user.id, :address => address)
+        flag = false if not User.update(current_user.id, :address => address)
     end
     if zip_code != ""
-        User.update(current_user.id, :zip_code => zip_code)
+         flag = false if not User.update(current_user.id, :zip_code => zip_code)
     end
     if neighborhood != ""
-        User.update(current_user.id, :neighborhood => neighborhood)
+         flag = false if not User.update(current_user.id, :neighborhood => neighborhood)
     end
     if city_id != ""
-        User.update(current_user.id, :city_id => city_id)
+         flag = false if not User.update(current_user.id, :city_id => city_id)
     end
+    return flag
   end
 
   def save_image_profile
-        User.update(current_user.id, :image_path => "/assets/" +  @user.name + "_img_profile.jpg")
+        User.update(current_user.id, :image_path => "/assets/" +  "img_profile_#{@picture_hash}.jpg")
   end
 end
