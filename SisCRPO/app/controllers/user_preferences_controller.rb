7da77c67a8f6af7932aaeff3e1abf9069b3fdc99
IA <c4rlo4@gmail.com>
2016-11-21 22:31:54 -0200

    quero ver você não chorar, não olhar para trás e nem se arrepender do que faz..

--- SisCRPO/app/controllers/user_preferences_controller.rb
+++ SisCRPO/app/controllers/user_preferences_controller.rb
@@ -12,10 +12,14 @@ class UserPreferencesController < ApplicationController
     @zip_code = params[:zip_code]
     @neighborhood = params[:neighborhood]
     @city_id = params[:city_id]
-    @picture = params[:picture]
-    File.open(Rails.root.join('app','assets', 'images', @user.name + "_img_profile.jpg"), 'wb') do |f|
-        f.write(@picture.read)
+    if params[:picture] != nil
+        @picture = params[:picture]
+        File.open(Rails.root.join('app','assets', 'images', @user.name + "_img_profile.jpg"), 'wb') do |f|
+            f.write(@picture.read)
+        end
+        save_image_profile
     end
+
 
     if save_params(@address, @zip_code, @neighborhood, @city_id)
         redirect_to dashboard_index_path notice: "Alterações salvas com sucesso!"
@@ -24,11 +28,21 @@ class UserPreferencesController < ApplicationController
 
   private
   def save_params(address, zip_code, neighborhood, city_id)
-    User.update(current_user.id, :address => address,
-                                             :zip_code => zip_code,
-                                             :neighborhood => neighborhood,
-                                             :city_id => city_id,
-                                             :image_path => "/assets/" +  @user.name + "_img_profile.jpg")
+    if address != ""
+        User.update(current_user.id, :address => address)
+    end
+    if zip_code != ""
+        User.update(current_user.id, :zip_code => zip_code)
+    end
+    if neighborhood != ""
+        User.update(current_user.id, :neighborhood => neighborhood)
+    end
+    if city_id != ""
+        User.update(current_user.id, :city_id => city_id)
+    end
   end
 
+  def save_image_profile
+        User.update(current_user.id, :image_path => "/assets/" +  @user.name + "_img_profile.jpg")
+  end
 end
